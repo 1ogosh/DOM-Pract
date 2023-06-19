c6cc0a594ca51b35a9a06a638868724438c99c54
Andrew Salmanov <ads739912@gmail.com>
2023-06-19 22:21:10 +0300

    getting some elements and statements

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.guess-message').textContent);
+
+document.querySelector('.check').addEventListener('click', function () {
+        const guessingNumber = Number(document.
+                querySelector('.number-input').value);
+        console.log(guessingNumber, typeof guessingNumber);
+
+        if (!guessingNumber) {
+                document.querySelector('.guess-message').
+                        textContent = 'Введите число';
+        }
+});
 
 
-document.querySelector('.guess-message').textContent = 'Правильно';
