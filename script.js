5a5e981bf0dee8cb02cdadfa31b454cbf96b163e
Andrew Salmanov <ads739912@gmail.com>
2023-06-20 19:12:45 +0300

    Logical improvements

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+console.log(secretNumber);
+
+document.querySelector('.question').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
         const guessingNumber = Number(document.
@@ -9,6 +15,28 @@ document.querySelector('.check').addEventListener('click', function () {
         if (!guessingNumber) {
                 document.querySelector('.guess-message').
                         textContent = 'Введите число';
+        } else if (guessingNumber === secretNumber) {
+                document.querySelector('.guess-message').textContent = 'Правильно';
+        } else if (guessingNumber > secretNumber) {
+
+                if (score > 1) {
+                        document.querySelector('.guess-message').textContent = 'Слишком большое число';
+                        score--;
+                        document.querySelector('.score').textContent = score;
+                } else {
+                        document.querySelector('.guess-message').textContent = 'Game Over';
+                }
+
+
+        } else if (guessingNumber < secretNumber) {
+                if (score > 1) {
+                        document.querySelector('.guess-message').textContent = 'Слишком маленькое число';
+                        score--;
+                        document.querySelector('.score').textContent = score;
+                } else {
+                        document.querySelector('.guess-message').textContent = 'Game Over';
+                        document.querySelector('.score').textContent = 0;
+                }
         }
 });
 
